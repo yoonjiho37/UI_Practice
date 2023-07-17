2760b7b42b3c4a609a4c114f550a7d37e480a6bb
yoonjiho37 <yoonjiho37@github.com>
2023-07-17 20:51:13 +0900

    [add] Git Ignore

---
+++ SwiftUI_Example/SwiftUI_Example/SwiftUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExampleApp.swift
+//  SwiftUI_Example
+//
+//  Created by 윤지호 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
